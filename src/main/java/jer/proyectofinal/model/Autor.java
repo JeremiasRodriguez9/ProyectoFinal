298b4017e715a7896543a850d2eb40e471b94ad3
Jeremias Rodriguez <rodriguezjeremias09@gmail.com>
2023-10-28 23:12:50 -0300

    cambios en modelo

--- src/main/java/jer/proyectofinal/model/Autor.java
+++ src/main/java/jer/proyectofinal/model/Autor.java
@@ -1,7 +1,17 @@
 package jer.proyectofinal.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
 
+@Entity
+@Table
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Autor {
     @Id
     private String id;
--- src/main/java/jer/proyectofinal/model/Editorial.java
+++ src/main/java/jer/proyectofinal/model/Editorial.java
@@ -1,8 +1,18 @@
 package jer.proyectofinal.model;
 
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
 
+@Entity
+@Table
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Editorial {
     @Id
     private String id;
